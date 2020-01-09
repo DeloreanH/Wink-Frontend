3f0300b861ef34b6e4780daf25f00e41fab7fadb
Harry Perez <harryy1242@gmail.com>
2020-01-09 11:52:46 -0400

    loader

--- src/app/core/services/loader.service.ts
+++ src/app/core/services/loader.service.ts
@@ -25,34 +25,34 @@ export class LoaderService  {
     ) { }
 
   async Show(opts?: LoadingOptions) {
-    try {
-      this.Close();
-      if (!this.Status) {
-        const optsO: LoadingOptions = {
-          message: 'WINK.LOADER.LOADING',
-          spinner: 'bubbles'
-        };
-        opts = opts ? opts : optsO;
-        opts.message = opts.message ? this.translateService.instant(opts.message) : null;
-        const loading = await this.loadingController.create(opts);
-        this.status = true;
-        await loading.present();
-      }
-    } catch (err) {
-      console.log('Error Show ', err);
-    }
+    // try {
+    //   this.Close();
+    //   if (!this.Status) {
+    //     const optsO: LoadingOptions = {
+    //       message: 'WINK.LOADER.LOADING',
+    //       spinner: 'bubbles'
+    //     };
+    //     opts = opts ? opts : optsO;
+    //     opts.message = opts.message ? this.translateService.instant(opts.message) : null;
+    //     const loading = await this.loadingController.create(opts);
+    //     this.status = true;
+    //     await loading.present();
+    //   }
+    // } catch (err) {
+    //   console.log('Error Show ', err);
+    // }
   }
   public async Close() {
-    try {
-      console.log('cerrar...');
-      if (await this.loadingController.getTop()) {
-        console.log('cerrar');
-        this.loadingController.dismiss();
-      }
-    } catch (err) {
-      this.loadingController.dismiss();
-    }
-    this.status = false;
+    // try {
+    //   console.log('cerrar...');
+    //   if (await this.loadingController.getTop()) {
+    //     console.log('cerrar');
+    //     this.loadingController.dismiss();
+    //   }
+    // } catch (err) {
+    //   this.loadingController.dismiss();
+    // }
+    // this.status = false;
   }
 
   public get Status(): boolean {
