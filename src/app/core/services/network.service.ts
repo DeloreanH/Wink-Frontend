2d8ae80d7fdbbd8d7b75b742d66f481eb667d327
Jessica Salazar <jessisr93@gmail.com>
2020-01-16 08:51:52 -0400

    message down network

--- src/app/core/services/network.service.ts
+++ src/app/core/services/network.service.ts
@@ -3,12 +3,17 @@ import { Network } from '@ionic-native/network/ngx';
 import { Platform } from '@ionic/angular';
 import { fromEvent, merge, of, Observable } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
+import { ToastService, PositionToast } from './toast.service';
 
 @Injectable()
 export class NetworkService {
   private online: Observable<boolean> =  new Observable();
 
-  constructor(public network: Network, public platform: Platform) {
+  constructor(
+    public network: Network,
+    public platform: Platform,
+    private toastService: ToastService,
+    ) {
     if (this.platform.is('cordova')) {
       // on Device
       this.online = merge(
@@ -23,6 +28,7 @@ export class NetworkService {
         fromEvent(window, 'offline').pipe(mapTo(false))
       );
     }
+    this.NetworkDownMessage();
   }
 
     public getNetworkType(): string {
@@ -31,5 +37,25 @@ export class NetworkService {
 
     public getNetworkStatus(): Observable<boolean> {
       return this.online;
+    }
+
+    private NetworkDownMessage() {
+      this.getNetworkStatus().subscribe(
+        (status) => {
+          if (!status) {
+            if (this.platform.is('cordova')) {
+              this.toastService.Toast('Sin internet.', null, null, PositionToast.BOTTOM, null, 'danger');
+            } else {
+              window.alert('Sin internet');
+            }
+          } else {
+            if (this.platform.is('cordova')) {
+              this.toastService.Toast('Reconectando', null, null, PositionToast.BOTTOM, null, 'primary');
+            } else {
+              window.alert('Reconectando');
+            }
+          }
+        }
+      );
     }
 }
